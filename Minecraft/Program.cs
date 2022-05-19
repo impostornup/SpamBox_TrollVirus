c9dbe42ff938f9954cc072e2dafda425199c6d6e
impostornup <janekintell0@gmail.com>
2022-05-19 18:37:20 +0200

    main
    shit code in 10minutes c#

---
+++ Minecraft/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minecraft
+{
+    internal class Program
+    {
+        public static int message;
+        static void Main(string[] args)
+        {
+            
+            while(true)
+            {
+                Thread th = new Thread(() =>
+                {
+
+                    message++;
+                    MessageBox.Show("russian hacked your computer!", "new message from putin (" + message + ")", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    
+
+
+                });
+                th.Start();
+                Thread.Sleep(1000);
+            }
+            
+
+
+
+
+        }
+        
+        
+    }
+}
